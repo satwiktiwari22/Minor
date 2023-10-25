ca8e8a1ef7764e0fec91cca6563446ba7937d583
somtyagi20 <somnathtyagi210787@acropolis.in>
2023-10-25 17:38:52 +0530

    Chat APIs

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import userRoutes from "./Routes/userRoutes.js";
+import chatRoutes from "./Routes/chatRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import cors from "cors";
 
@@ -12,6 +13,8 @@ connectDB();
 
 app.use(express.json());
 app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
+
 app.use(notFound);
 app.use(errorHandler);
 
