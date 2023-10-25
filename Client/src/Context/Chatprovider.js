81c99171b9fc8c922b80106207b785df4572d073
satwiktiwari22 <satwiktiwari210196@acropolis.in>
2023-10-25 17:41:59 +0530

    Search at client

---
+++ Client/src/Context/Chatprovider.js
@@ -0,0 +1,35 @@
+import { createContext, useContext, useEffect, useState } from "react";
+const ChatContext = createContext();
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useNavigate,
+} from "react-router-dom";
+
+const ChatProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const userInfo = localStorage.getItem("user");
+    if (userInfo) {
+      setUser(JSON.parse(userInfo));
+    } else {
+      setUser(null);
+      navigate("/desktop-2");
+    }
+  }, []);
+
+  return (
+    <ChatContext.Provider value={{ user, setUser }}>
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const ChatState = () => {
+  return useContext(ChatContext);
+};
+// useContext(ChatContext);
+
+export default ChatProvider;
